d044691385e309bd1c2239f811e5fcbf4be0bc27
Erik Grundy <erik.grundy@gmail.com>
2024-01-29 00:06:14 +1300

    enable saving and opening files

--- ratatui-experiment/src/main.rs
+++ ratatui-experiment/src/main.rs
@@ -1,5 +1,7 @@
 use std::{
-    io::{self, Stdout},
+    fs::{self, File},
+    io::{self, Stdout, Write},
+    path::Path,
     time::Duration,
 };
 
@@ -34,38 +36,132 @@ fn restore_terminal(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result
 
 #[derive(Default, Debug)]
 struct App {
+    current_file_name: Option<String>,
+    open_file_name: Option<String>,
+    input_destination: InputDestination,
     file_contents: String,
+}
+
+impl App {
+    fn accept_input(&mut self, input: Input) -> bool {
+        match input {
+            Input::None => (),
+            Input::Backspace => self.backspace(),
+            Input::Enter => self.enter(),
+            Input::Cancel => return false,
+            Input::NormalChar(c) => self.add_char(c),
+            Input::Save => self.input_destination = InputDestination::Save,
+            Input::Open => self.input_destination = InputDestination::Open,
+        }
+        true
+    }
+
+    fn backspace(&mut self) {
+        match self.input_destination {
+            InputDestination::Buffer => _ = self.file_contents.pop(),
+            InputDestination::Open => {
+                if let Some(open) = &mut self.open_file_name {
+                    _ = open.pop()
+                }
+            }
+            InputDestination::Save => {
+                if let Some(current) = &mut self.current_file_name {
+                    _ = current.pop()
+                }
+            }
+        }
+    }
+
+    fn enter(&mut self) {
+        match self.input_destination {
+            InputDestination::Buffer => self.file_contents.push('\n'),
+            InputDestination::Save => {
+                // TODO: get rid of all these unwraps
+                let path = Path::new(self.current_file_name.as_ref().unwrap());
+                let mut file = File::create(path).unwrap();
+                file.write_all(self.file_contents.as_bytes()).unwrap();
+                self.input_destination = InputDestination::Buffer;
+            }
+            InputDestination::Open => {
+                // TODO: get rid of all these unwraps
+                let path = self.open_file_name.as_ref().unwrap();
+                self.file_contents = fs::read_to_string(path).unwrap();
+                self.current_file_name = self.open_file_name.take();
+                self.input_destination = InputDestination::Buffer;
+            }
+        }
+    }
+
+    fn add_char(&mut self, c: char) {
+        match self.input_destination {
+            InputDestination::Buffer => self.file_contents.push(c),
+            InputDestination::Open => self.open_file_name.get_or_insert_with(String::new).push(c),
+            InputDestination::Save => self
+                .current_file_name
+                .get_or_insert_with(String::new)
+                .push(c),
+        }
+    }
+}
+
+#[derive(Debug, Default, PartialEq, Eq)]
+enum InputDestination {
+    #[default]
+    Buffer,
+    Open,
+    Save,
 }
 
 fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<()> {
     let mut app = App::default();
-    loop {
+    let mut should_continue = true;
+    while should_continue {
         terminal.draw(|frame| render_app(frame, &app))?;
-        match handle_input()? {
-            Input::None => (),
-            Input::Backspace => _ = app.file_contents.pop(),
-            Input::Cancel => break,
-            Input::NormalChar(c) => app.file_contents.push(c),
-        }
+        should_continue = app.accept_input(get_input()?);
     }
     Ok(())
 }
 
-fn render_app(frame: &mut Frame<CrosstermBackend<Stdout>>, app: &App) {
+fn render_app(frame: &mut Frame, app: &App) {
+    let block = Block::default().borders(Borders::all());
+    let size = frame.size();
     let text = Paragraph::new(app.file_contents.clone())
-        .block(Block::default().borders(Borders::all()))
+        .block(block.clone())
         .wrap(Wrap { trim: true });
-    frame.render_widget(text, frame.size());
+    if let Some(message) = get_message(app) {
+        let layout = Layout::new(Direction::Vertical, Constraint::from_mins([3, 0])).split(size);
+        frame.render_widget(Paragraph::new(message).block(block), layout[0]);
+        frame.render_widget(text, layout[1]);
+    } else {
+        frame.render_widget(text, size);
+    }
+}
+
+fn get_message(app: &App) -> Option<String> {
+    match app.input_destination {
+        InputDestination::Buffer => None,
+        InputDestination::Open => Some(format!(
+            "Open file: {}",
+            app.open_file_name.as_deref().unwrap_or("")
+        )),
+        InputDestination::Save => Some(format!(
+            "Save as: {}",
+            app.current_file_name.as_deref().unwrap_or("")
+        )),
+    }
 }
 
 enum Input {
     None,
     Backspace,
+    Enter,
     Cancel,
     NormalChar(char),
+    Save,
+    Open,
 }
 
-fn handle_input() -> Result<Input> {
+fn get_input() -> Result<Input> {
     if event::poll(Duration::from_millis(250)).context("event poll failed")? {
         if let Event::Key(event) = event::read().context("event read failed")? {
             return get_char(event);
@@ -82,13 +178,23 @@ fn get_char(event: KeyEvent) -> Result<Input> {
             ..
         } => Ok(Input::Cancel),
         KeyEvent {
+            code: KeyCode::Char('o'),
+            modifiers: KeyModifiers::CONTROL,
+            ..
+        } => Ok(Input::Open),
+        KeyEvent {
+            code: KeyCode::Char('s'),
+            modifiers: KeyModifiers::CONTROL,
+            ..
+        } => Ok(Input::Save),
+        KeyEvent {
             code: KeyCode::Backspace,
             ..
         } => Ok(Input::Backspace),
         KeyEvent {
             code: KeyCode::Enter,
             ..
-        } => Ok(Input::NormalChar('\n')),
+        } => Ok(Input::Enter),
         KeyEvent {
             code: KeyCode::Char(c),
             ..
