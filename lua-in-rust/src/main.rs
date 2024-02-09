b2606bba139afad3a04dc3c9dc80eb6d9e1ec9ef
Erik Grundy <erik.grundy@gmail.com>
2024-02-10 11:49:30 +1300

    tidy up

--- lua-in-rust/src/main.rs
+++ lua-in-rust/src/main.rs
@@ -1,34 +1,19 @@
 use rlua::{Lua, MetaMethod, ToLua, UserData, Value};
 
 fn main() {
+    let program = r#"
+        print(options.a, options.b)
+        options.a = 17
+        print(options.a, options.b)
+    "#;
     let lua = Lua::new();
     let result: rlua::Result<_> = lua.context(|context| {
         context.scope(|scope| {
-            let val = Options::default();
-            context.globals().set("options", val)?;
-            context
-                .load(
-                    r#"
-                print(options.a, options.b)
-                options.a = 17
-                print(options.a, options.b)
-                "#,
-                )
-                .exec()?;
-            println!("from rust: {:?}", val);
-            // let userdata = scope.create_static_userdata(Options::default())?;
-            // context.globals().set("options", userdata.clone())?;
-            // context
-            //     .load(
-            //         r#"
-            //     print(options.a, options.b)
-            //     options.a = 17
-            //     print(options.a, options.b)
-            //     "#,
-            //     )
-            //     .exec()?;
-            // let new_options = userdata.borrow::<Options>()?;
-            // println!("from rust: {:?}", new_options);
+            let userdata = scope.create_static_userdata(Options::default())?;
+            context.globals().set("options", userdata.clone())?;
+            context.load(program).exec()?;
+            let new_options = userdata.borrow::<Options>()?;
+            println!("from rust: {:?}", new_options);
             Ok(())
         })?;
         Ok(())
@@ -58,8 +43,8 @@ impl UserData for Options {
         methods.add_meta_method(MetaMethod::Index, |context, options, key: String| match key
             .as_str()
         {
-            "a" => Ok(options.a.to_lua(context)),
-            "b" => Ok(options.b.clone().to_lua(context)),
+            "a" => Ok(options.a.to_lua(context)?),
+            "b" => Ok(options.b.clone().to_lua(context)?),
             _ => Err(rlua::Error::external(format!("unknown field '{}'", key))),
         });
         methods.add_meta_method_mut(
