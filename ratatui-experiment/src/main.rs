c225f19c1f6f6481b374a9767f024ff9c06b1ddf
Erik Grundy <erik.grundy@gmail.com>
2024-02-10 19:53:42 +1300

    break state out into its own file

--- ratatui-experiment/src/main.rs
+++ ratatui-experiment/src/main.rs
@@ -1,4 +1,6 @@
-use std::{fmt::Display, fs, io, time::Duration};
+mod state;
+
+use std::{io, time::Duration};
 
 use anyhow::{Context, Error, Result};
 use crossterm::{
@@ -7,6 +9,8 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use ratatui::{prelude::*, widgets::*};
+
+use state::{App, Input, InputDestination};
 
 fn main() -> Result<()> {
     setup_panics();
@@ -44,152 +48,6 @@ fn restore_terminal() -> Result<()> {
     Ok(())
 }
 
-#[derive(Default, Debug)]
-struct App {
-    current_file_name: Option<String>,
-    open_file_name: Option<String>,
-    input_destination: InputDestination,
-    file_contents: String,
-    messages: Vec<String>,
-    message_visible: bool,
-}
-
-impl App {
-    fn accept_input(&mut self, input: Input) -> bool {
-        match input {
-            Input::None => (),
-            Input::Backspace => self.backspace(),
-            Input::Enter => {
-                if let Err(err) = self.enter() {
-                    self.add_message(err.to_string());
-                }
-            }
-            Input::Cancel => return false,
-            Input::NormalChar(c) => self.add_char(c),
-            Input::Save => self.input_destination = InputDestination::Save,
-            Input::Open => self.input_destination = InputDestination::Open,
-            Input::New => self.new_file(),
-            Input::ClearMessage => self.clear_message(),
-        }
-        true
-    }
-
-    fn backspace(&mut self) {
-        match self.input_destination {
-            InputDestination::Buffer => _ = self.file_contents.pop(),
-            InputDestination::Open => {
-                if let Some(open) = &mut self.open_file_name {
-                    _ = open.pop()
-                }
-            }
-            InputDestination::Save => {
-                if let Some(current) = &mut self.current_file_name {
-                    _ = current.pop()
-                }
-            }
-        }
-    }
-
-    fn enter(&mut self) -> Result<(), EditorError> {
-        match self.input_destination {
-            InputDestination::Buffer => {
-                self.file_contents.push('\n');
-                Ok(())
-            }
-            InputDestination::Save => {
-                let result = self.save_file();
-                self.input_destination = InputDestination::Buffer;
-                self.clear_message();
-                result
-            }
-            InputDestination::Open => {
-                let result = self.open_file();
-                self.input_destination = InputDestination::Buffer;
-                self.clear_message();
-                result
-            }
-        }
-    }
-
-    fn save_file(&mut self) -> Result<(), EditorError> {
-        let path = self
-            .current_file_name
-            .as_ref()
-            .ok_or(EditorError::NoFileSpecified)?;
-        fs::write(path, self.file_contents.as_bytes())?;
-        Ok(())
-    }
-
-    fn open_file(&mut self) -> Result<(), EditorError> {
-        let path = self
-            .open_file_name
-            .take()
-            .ok_or(EditorError::NoFileSpecified)?;
-        self.file_contents = fs::read_to_string(&path)?;
-        self.current_file_name = Some(path);
-        Ok(())
-    }
-
-    fn add_char(&mut self, c: char) {
-        match self.input_destination {
-            InputDestination::Buffer => self.file_contents.push(c),
-            InputDestination::Open => self.open_file_name.get_or_insert_with(String::new).push(c),
-            InputDestination::Save => self
-                .current_file_name
-                .get_or_insert_with(String::new)
-                .push(c),
-        }
-    }
-
-    fn new_file(&mut self) {
-        self.file_contents.clear();
-        self.current_file_name = None;
-    }
-
-    fn add_message(&mut self, message: String) {
-        self.messages.push(message);
-        self.message_visible = true;
-    }
-
-    fn clear_message(&mut self) {
-        self.message_visible = false;
-    }
-}
-
-#[derive(Debug)]
-enum EditorError {
-    NoFileSpecified,
-    IoError(io::Error),
-}
-
-impl From<io::Error> for EditorError {
-    fn from(value: io::Error) -> Self {
-        Self::IoError(value)
-    }
-}
-
-impl Display for EditorError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            EditorError::NoFileSpecified => f.write_str("No file specified"),
-            EditorError::IoError(err) => {
-                f.write_str("IO error: ")?;
-                err.fmt(f)
-            }
-        }
-    }
-}
-
-impl std::error::Error for EditorError {}
-
-#[derive(Debug, Default, PartialEq, Eq)]
-enum InputDestination {
-    #[default]
-    Buffer,
-    Open,
-    Save,
-}
-
 fn render_app(frame: &mut Frame, app: &App) {
     let mut content_box = frame.size();
     let text = paragraph_with_block(
@@ -201,7 +59,7 @@ fn render_app(frame: &mut Frame, app: &App) {
         let dialogue_box = break_off_top(&mut content_box, 3);
         frame.render_widget(dialogue, dialogue_box);
     }
-    if let Some(error) = app.messages.last().filter(|_| app.message_visible) {
+    if let Some(error) = app.latest_message() {
         let error_box = break_off_top(&mut content_box, 3);
         frame.render_widget(paragraph_with_block("Error", error), error_box);
     }
@@ -230,18 +88,6 @@ fn get_message(app: &App) -> Option<Paragraph<'_>> {
 
 fn paragraph_with_block<'a>(block_title: &'a str, content: &'a str) -> Paragraph<'a> {
     Paragraph::new(content).block(Block::default().borders(Borders::all()).title(block_title))
-}
-
-enum Input {
-    None,
-    Backspace,
-    Enter,
-    Cancel,
-    NormalChar(char),
-    Save,
-    Open,
-    New,
-    ClearMessage,
 }
 
 fn get_input() -> Result<Input> {
---
+++ ratatui-experiment/src/state.rs
@@ -0,0 +1,167 @@
+use std::{fmt::Display, fs, io};
+
+#[derive(Default, Debug)]
+
+pub struct App {
+    pub current_file_name: Option<String>,
+    pub open_file_name: Option<String>,
+    pub input_destination: InputDestination,
+    pub file_contents: String,
+    messages: Vec<String>,
+    message_visible: bool,
+}
+
+impl App {
+    pub fn accept_input(&mut self, input: Input) -> bool {
+        match input {
+            Input::None => (),
+            Input::Backspace => self.backspace(),
+            Input::Enter => {
+                if let Err(err) = self.enter() {
+                    self.add_message(err.to_string());
+                }
+            }
+            Input::Cancel => return false,
+            Input::NormalChar(c) => self.add_char(c),
+            Input::Save => self.input_destination = InputDestination::Save,
+            Input::Open => self.input_destination = InputDestination::Open,
+            Input::New => self.new_file(),
+            Input::ClearMessage => self.clear_message(),
+        }
+        true
+    }
+
+    fn backspace(&mut self) {
+        match self.input_destination {
+            InputDestination::Buffer => _ = self.file_contents.pop(),
+            InputDestination::Open => {
+                if let Some(open) = &mut self.open_file_name {
+                    _ = open.pop()
+                }
+            }
+            InputDestination::Save => {
+                if let Some(current) = &mut self.current_file_name {
+                    _ = current.pop()
+                }
+            }
+        }
+    }
+
+    fn enter(&mut self) -> Result<(), EditorError> {
+        match self.input_destination {
+            InputDestination::Buffer => {
+                self.file_contents.push('\n');
+                Ok(())
+            }
+            InputDestination::Save => {
+                let result = self.save_file();
+                self.input_destination = InputDestination::Buffer;
+                self.clear_message();
+                result
+            }
+            InputDestination::Open => {
+                let result = self.open_file();
+                self.input_destination = InputDestination::Buffer;
+                self.clear_message();
+                result
+            }
+        }
+    }
+
+    fn save_file(&mut self) -> Result<(), EditorError> {
+        let path = self
+            .current_file_name
+            .as_ref()
+            .ok_or(EditorError::NoFileSpecified)?;
+        fs::write(path, self.file_contents.as_bytes())?;
+        Ok(())
+    }
+
+    fn open_file(&mut self) -> Result<(), EditorError> {
+        let path = self
+            .open_file_name
+            .take()
+            .ok_or(EditorError::NoFileSpecified)?;
+        self.file_contents = fs::read_to_string(&path)?;
+        self.current_file_name = Some(path);
+        Ok(())
+    }
+
+    fn add_char(&mut self, c: char) {
+        match self.input_destination {
+            InputDestination::Buffer => self.file_contents.push(c),
+            InputDestination::Open => self.open_file_name.get_or_insert_with(String::new).push(c),
+            InputDestination::Save => self
+                .current_file_name
+                .get_or_insert_with(String::new)
+                .push(c),
+        }
+    }
+
+    fn new_file(&mut self) {
+        self.file_contents.clear();
+        self.current_file_name = None;
+    }
+
+    fn add_message(&mut self, message: String) {
+        self.messages.push(message);
+        self.message_visible = true;
+    }
+
+    fn clear_message(&mut self) {
+        self.message_visible = false;
+    }
+
+    pub fn latest_message(&self) -> Option<&str> {
+        self.messages
+            .last()
+            .filter(|_| self.message_visible)
+            .map(String::as_str)
+    }
+}
+
+#[derive(Debug, Default, PartialEq, Eq)]
+pub enum InputDestination {
+    #[default]
+    Buffer,
+    Open,
+    Save,
+}
+
+pub enum Input {
+    None,
+    Backspace,
+    Enter,
+    Cancel,
+    NormalChar(char),
+    Save,
+    Open,
+    New,
+    ClearMessage,
+}
+
+#[derive(Debug)]
+enum EditorError {
+    NoFileSpecified,
+    IoError(io::Error),
+}
+
+impl From<io::Error> for EditorError {
+    fn from(value: io::Error) -> Self {
+        Self::IoError(value)
+    }
+}
+
+impl Display for EditorError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            EditorError::NoFileSpecified => f.write_str("No file specified"),
+            EditorError::IoError(err) => {
+                f.write_str("IO error: ")?;
+                err.fmt(f)
+            }
+        }
+    }
+}
+
+impl std::error::Error for EditorError {}
